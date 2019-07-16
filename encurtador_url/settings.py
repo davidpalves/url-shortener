e8cbe3a2f83560f641935c6e3681c313a79e4199
David Pierre <davidpierrealves21@gmail.com>
2019-07-16 11:47:10 -0300

    Fix .env

--- encurtador_url/settings.py
+++ encurtador_url/settings.py
@@ -1,6 +1,6 @@
 import os
 import django_heroku
-from decouple import config
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
