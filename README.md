eb22f4d37f800c966f55ffe4f74d9a3f364c3a24
David Pierre <davidpierrealves21@gmail.com>
2019-07-16 11:33:20 -0300

    Add python-decouple and deploy to heroku button

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Django Application for URL Shortening
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- encurtador_url/settings.py
+++ encurtador_url/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_heroku
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,10 +9,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!q6&-^1axh%yzoxp1yw3*1jmqayfjfp%z2epeyv_2dr8a=0894'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
